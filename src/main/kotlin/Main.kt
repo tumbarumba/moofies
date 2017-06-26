36cc849f7715de34bab531ab93977b53ca87fbda
Joe Schmetzer <joe@exubero.com>
2017-06-26 22:02:55 +0100

    Show movie and genre details

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,27 @@
 import org.neo4j.driver.v1.AuthTokens
-import org.neo4j.driver.v1.Config
 import org.neo4j.driver.v1.GraphDatabase
 import org.neo4j.driver.v1.Values.parameters
 
 fun main(args: Array<String>) {
-    val noSSL = Config.build().withEncryptionLevel(Config.EncryptionLevel.NONE).toConfig()
-    val driver = GraphDatabase.driver("bolt://34.239.249.221:33516", AuthTokens.basic("neo4j", "windings-hills-perforators"), noSSL) // <password>
+    val driver = GraphDatabase.driver("bolt://54.145.155.174:32909", AuthTokens.basic("neo4j", "buoys-thread-task"))
 
     driver.session().use { session ->
         val cypherQuery ="""
-            MATCH (n)
-            RETURN id(n) AS id
+            MATCH (m:Movie) --> (g:Genre)
+            RETURN properties(m) as movie, g.name as genre
             LIMIT ${'$'}limit
         """
-        val result = session.run(cypherQuery, parameters("limit", 10))
+        val result = session.run(cypherQuery, parameters("limit", 100))
         while (result.hasNext()) {
-            println(result.next().get("id"))
+            val record = result.next()
+            val movie = record.get("movie")
+            val genre = record.get("genre")
+            val title = movie.get("title")
+            println("$title ($genre)")
+
+            for (key in movie.keys().sorted()) {
+                println("\t$key:\t${movie.get(key)}")
+            }
         }
     }
 }
